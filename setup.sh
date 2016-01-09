9e20aca3b4eea43ced84f9f5003c61d22069e7c1
jonasdaugalas <jonasdaugalas@gmail.com>
2016-01-09 03:58:26 +0100

    Fix symlink setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ln -sf emacs/.emacs.d/personal/*.el ~/.emacs.d/personal/
-ln -sf emacs/.emacs.d/personal/preload/* ~/.emacs.d/personal/preload/
+ln -sf --relative ./emacs/.emacs.d/prelude-modules.el ~/.emacs.d/prelude-modules.el
+ln -sf --relative ./emacs/.emacs.d/personal/*.el ~/.emacs.d/personal/
+ln -sf --relative ./emacs/.emacs.d/personal/preload/* ~/.emacs.d/personal/preload/
