556d3c197a40cda1b07577352396e63de07df1c6
jonasdaugalas <jonasdaugalas@gmail.com>
2016-04-10 15:16:37 +0300

    Fix setup script for spacemacs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-ln -sf --relative ./emacs/.emacs.d/prelude-modules.el ~/.emacs.d/prelude-modules.el
-ln -sf --relative ./emacs/.emacs.d/personal/*.el ~/.emacs.d/personal/
-ln -sf --relative ./emacs/.emacs.d/personal/preload/* ~/.emacs.d/personal/preload/
+EMACS_PRIVATE=~/.emacs.d/private
+if [ -d "$EMACS_PRIVATE" ]; then
+    if [ -L "$EMACS_PRIVATE" ]; then
+        # It is a symlink!
+        echo removing symlink $EMACS_PRIVATE
+        rm "$EMACS_PRIVATE"
+    else
+        # It's a directory!
+        echo $EMACS_PRIVATE does not look like symlink
+        exit
+    fi
+fi
+echo linking $EMACS_PRIVATE
+ln -sf --relative ./emacs/.emacs.d/private/ ~/.emacs.d/private
+echo linking .spacemacs
+ln -sf --relative ./emacs/.spacemacs ~/.spacemacs
