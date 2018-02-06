c2fe25d78371cd1566faf09f84e24747bad1aa68
jonas <jonas@jdhost.cern.ch>
2018-02-06 12:05:36 +0100

    Add tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
-set PATH /home/jonas/local/bin $PATH
+if status is-interactive
+    and not set -q TMUX
+    exec tmux
+end
+# set PATH /home/jonas/local/bin $PATH
 # export ANDROID_HOME=/opt/android/
 # export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
