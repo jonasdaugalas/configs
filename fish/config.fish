97aa80a2d372e529c62bae2ba143e5ab1fcd4f83
jonasdaugalas <jonasdaugalas@gmail.com>
2018-12-16 20:32:44 +0100

    Add exit from fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-if status is-interactive
-    and not set -q TMUX
-    exec tmux
-end
 # set PATH /home/jonas/local/bin $PATH
 # export ANDROID_HOME=/opt/android/
 # export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+if status is-interactive
+    and not set -q TMUX
+    tmux
+end
