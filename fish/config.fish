bcb0f7af470530aecb229aed63a42f926a403b2f
Jonas Daugalas <jdaugalas@simscale.com>
2019-04-15 18:09:52 +0200

    Update PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,3 @@
-set PATH /home/jonas/local/bin:/home/jonas/.yarn/bin $PATH
-set PATH $PATH /usr/local/go/bin:/home/jonas/go/bin
+set PATH {HOME}/local/bin {HOME}/.yarn/bin $PATH {HOME}/go/bin {HOME}/go/bin
 set PKG_CONFIG_PATH $PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 alias python=python3
