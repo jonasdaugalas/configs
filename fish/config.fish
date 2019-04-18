06cc7928dfc3a5b1eeb4783c33366cc3e60fe759
jdaugala <jonas.daugalas@cern.ch>
2019-04-18 16:49:09 +0300

    Update fish paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH $PATH /usr/local/go/bin {HOME}/local/bin {HOME}/.yarn/bin {HOME}/go/bin
+set PATH $PATH /usr/local/go/bin $HOME/local/bin $HOME/.yarn/bin $HOME/go/bin
 set PKG_CONFIG_PATH $PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 set LD_LIBRARY_PATH /usr/local/lib
 alias python=python3
